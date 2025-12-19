a89cc84e46c143c5bcb13702b4b15fd2d8da3927
harrybui <mr.flooo1230@gmail.com>
2025-12-19 18:02:49 +0700

    Update mail config

--- api-edtech/src/main/java/com/example/backend/BackendApplication.java
+++ api-edtech/src/main/java/com/example/backend/BackendApplication.java
@@ -2,6 +2,7 @@ package com.example.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
@@ -9,13 +10,15 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class BackendApplication {
 
 	public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(BackendApplication.class, args);
+
+        String resolvedUser = context.getEnvironment().getProperty("spring.mail.username");
+        String resolvedPass = context.getEnvironment().getProperty("spring.mail.password");
+        System.out.println("Application.yml mail " + resolvedUser + "pass " + resolvedPass);
+
         String mailUser = System.getenv("SPRING_MAIL_USERNAME");
         String mailPass = System.getenv("SPRING_MAIL_PASSWORD");
-
-        if (mailUser != null) System.setProperty("spring.mail.username", mailUser);
-        if (mailPass != null) System.setProperty("spring.mail.password", mailPass);
-
-		SpringApplication.run(BackendApplication.class, args);
+        System.out.println("Environment mail " + mailUser + "pass " + mailPass);
 	}
 
 }
