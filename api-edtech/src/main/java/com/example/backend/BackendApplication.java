7ac6cde2f7fc58a8eae8fe401be80499f165702d
harrybui <mr.flooo1230@gmail.com>
2025-12-19 17:05:40 +0700

    Update mail config

--- api-edtech/src/main/java/com/example/backend/BackendApplication.java
+++ api-edtech/src/main/java/com/example/backend/BackendApplication.java
@@ -9,6 +9,12 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class BackendApplication {
 
 	public static void main(String[] args) {
+        String mailUser = System.getenv("SPRING_MAIL_USERNAME");
+        String mailPass = System.getenv("SPRING_MAIL_PASSWORD");
+
+        if (mailUser != null) System.setProperty("spring.mail.username", mailUser);
+        if (mailPass != null) System.setProperty("spring.mail.password", mailPass);
+
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
